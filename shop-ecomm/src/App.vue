c03c9740449ca3d8646fab75f4de1a2e7f1b6fd0
Andrey Gavrilov <boombardir21@gmail.com>
2022-12-17 16:43:15 +0500

    start and settings, npm install -D sass

---
+++ shop-ecomm/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from 'vue';
+import Header from './components/Layout/Header.vue';
+
+export default defineComponent({
+  components: { Header },
+});
+</script>
+
+<template>
+  <div class='container'>
+    <Header />
+
+  </div>
+</template>
+
+<style lang='scss' scoped>
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+</style>
